787360046c56a4200fa526a77f0e432360e4a8e0
DuyVu <duyvu.b7@gmail.com>
2021-03-16 11:17:36 +0700

    q

--- src/main/java/fpt/repository/PostRepository.java
+++ src/main/java/fpt/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package fpt.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +12,9 @@ import fpt.model.Post;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
 	
-	@Query(value="select * from Post p where p.postId like :id", nativeQuery = true)
+	@Query(value="select * from Post p where p.postId like :id and deleteBy is null and postStatus like 'True'", nativeQuery = true)
 	Post findOneById( @Param("id") int id);
+	
+	@Query(value="select * from Post p where deleteBy is null and postStatus like '1'", nativeQuery = true)
+	List<Post> findAllAvailability();
 }
